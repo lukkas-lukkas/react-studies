c708012b3e086472175ac90f2369d2b87d4bfec7
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-28 19:40:17 -0300

    create first function component List

---
+++ src/components/List/index.tsx
@@ -0,0 +1,25 @@
+export default function List() {
+    return (
+        <aside>
+            <h2> Studies today </h2>
+            <ul>
+                <li>
+                    <h3>
+                        React
+                    </h3>
+                    <span>
+                        02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        Javascript
+                    </h3>
+                    <span>
+                        01:00:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
