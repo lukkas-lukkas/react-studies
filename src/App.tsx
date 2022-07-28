fab096c00514af1575260caf62a925e58b74e971
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-28 19:35:53 -0300

    create simple form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import Button from "./components/Button";
+import Form from './components/Form';
+
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Form />
     </div>
   );
 }
