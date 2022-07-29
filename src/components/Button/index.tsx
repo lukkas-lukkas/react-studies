bc2daaf940530959983bb1fb25271cd8ec72fe78
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-07-29 19:28:54 -0300

    convert button to function component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
 import './style.scss';
 
-class Button extends React.Component<{
+interface Props {
     text: string,
     type?: "button" | "submit" | "reset" | undefined,
     onClick?: () => void,
-}> {
-    render() {
-        const { type = 'button', onClick } = this.props;
+}
 
-        return (
-            <button className='button' type={type} onClick={onClick}>
-                {this.props.text}
-            </button>
-        )
-    }
+function Button({ text, type = 'button', onClick }: Props) {
+    return (
+        <button className='button' type={type} onClick={onClick}>
+            {text}
+        </button>
+    )
 }
 
 export default Button;
